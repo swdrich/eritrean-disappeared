21488d7b15ffd9522573d6488407d32a0ee308e2
Steph <swdrinkard@gmail.com>
2021-05-30 00:07:40 -0500

    Added some code to outline overlay maps. No maps displaying yet.

--- static/js/map.js
+++ static/js/map.js
@@ -1,8 +1,35 @@
 console.log("map.js is loaded!");
 
 
+// Define function to create features
+// function createFeatures() {
+
+
+    // Define function to create boundaries
+    function drawBorders() {
+        var link = "../resources/world-administrative-boundaries.geojson";
+
+        d3.json(link).then(function(data) {
+            console.log(data);
+    
+            var borders = L.geoJson(data, {
+                style: function(feature) {
+                    return {
+                        color: "black",
+                        weight: 2
+                    };    
+                },
+            })
+            return borders;
+        })
+    
+    }
+
+// }
+// createFeatures();
+
 // Define function to create map
-function createMap(prisons) {
+function createMap() {
 
     // Define satellite base map
     var satelliteMap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -14,20 +41,36 @@ function createMap(prisons) {
         accessToken: accessToken
     });
 
+    // Define administrative borders
+    var borders = d3.json("../resources/world-administrative-boundaries.geojson").then(function(data) {
+        console.log(data);
+    });
+    
+    // Define prison locations
+    var prisons = d3.json("../resources/eritrean_prisons.geojson").then(function(data){
+        console.log(data);
+    });
+        
     // Define a basemap object to hold our base maps
     var baseMaps = {
         "Satellite Map" : satelliteMap
+    };
+
+    // Define an overlaymap object to hold our overlay maps
+    var overlayMaps = {
+        "Administrative Boundaries": borders,
+        "Prisons": prisons
     };
 
     // Create map and define characteristic to load initially
     var myMap = L.map("mapid", {
         center: [15.18, 39.78],
         zoom: 8,
-        layers: [satelliteMap]
+        layers: [satelliteMap, borders, prisons]
     });
 
     // Create a layer control and pass in maps
-    L.control.layers(baseMaps, {
+    L.control.layers(baseMaps, overlayMaps, {
         collapsed: true
     }).addTo(myMap);
 
